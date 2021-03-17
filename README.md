d026d91e4413086b150d0dfa0dc753229384e198
Nick Huemmer <njhuemmer@gmail.com>
2021-03-16 17:47:05 -0700

    Starting to build out project - added classes and shell methods to each.  Also added experimental code to see how it works.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # game_show_app
+This is a game show app similar to Wheel of Fortune.  It's the fourth project in the Treehouse Full Stack JavaScript Techdegree. 
--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,36 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+    constructor(phrase) {
+        this.phrase = phrase.toLowerCase();
+    }
+    addPhraseToDisplay() {
+        //const addPhraseToDisplay = arr => {
+            const letterArray = getRandomPhraseAsArray();
+            const listElement = document.querySelector('#phrase ul');
+            const numberOfListItems = letterArray.length;
+        //this loop goes through and adds a list item with a letter for each letter from
+        //the random phrase.    
+            for (let i = 0; i < numberOfListItems; i++) {
+                let listItem = document.createElement('li');
+                listItem.textContent = letterArray [i];
+                phrase.appendChild(listItem);
+        //This if statement tests whether the character is a letter or space, then adds it
+        //as a classname.      
+                if(listItem.textContent !== ' ') {
+                    listItem.className = 'letter';
+                } else {
+                    listItem.className = 'space';
+                }
+            } 
+        //}
+    }
+    checkLetter() {
+
+    }
+    showMatchedLetter() {
+        
+    }
+}
